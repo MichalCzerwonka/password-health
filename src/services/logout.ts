cd27de4ff4420eb28b7481b7ee098663e4395a09
michalczerwonka <michal.czerwonka@ziegert-group.com>
2022-11-16 22:54:43 +0100

    bugfix: logout button fix

--- src/services/logout.ts
+++ src/services/logout.ts
@@ -1,5 +1,8 @@
-const logout = () => {
+import { Routes } from "~/constants";
 
+const logout = () => {
+  localStorage.removeItem('token');
+  window.location.href = Routes.Login;
 };
 
 export default logout;
