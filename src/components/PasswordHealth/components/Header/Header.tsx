62439be17f72724d108141ad910ebd36960f7646
michalczerwonka <michal.czerwonka@ziegert-group.com>
2022-11-17 20:47:07 +0100

    bugfix: fix vulnerable items calculation

--- src/components/PasswordHealth/components/Header/Header.tsx
+++ src/components/PasswordHealth/components/Header/Header.tsx
@@ -1,22 +1,28 @@
-import {FC} from 'react';
-import {IItem} from "~/services/getUserItems";
+import { FC } from 'react';
+import { IItem } from "~/services/getUserItems";
 import logout from '../../../../services/logout';
 
 import './header-style.scss';
+import itemHasOldPassword from "~/utils/itemHasOldPassword";
+import itemHasReusedPassword from "~/utils/itemHasReusedPassword";
+import itemHasWeakPassword from "~/utils/itemHasWeakPassword";
 
 interface IHeader {
   items: Array<IItem>;
   username: string;
 }
 
-const Header: FC<IHeader> = ({items, username}) => {
+const Header: FC<IHeader> = ({ items, username }) => {
+  const atLeastOneVulnerability = items.filter((item) =>
+    itemHasOldPassword(item) || itemHasReusedPassword(item, items) || itemHasWeakPassword(item)
+  ).length;
 
   return (
     <div className="header">
       <div className="user-section">
         <button onClick={logout}>{`Logout ${username}`}</button>
       </div>
-      <h1>{`${items.length} Items are vulnerable`}</h1>
+      <h1>{`${atLeastOneVulnerability} Items are vulnerable`}</h1>
       <span>Create new complex passwords to protect your accounts</span>
     </div>
   )
