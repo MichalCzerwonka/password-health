1383dd0363572db6fea2de7dd471a0cdf0a5e083
michalczerwonka <michal.czerwonka@ziegert-group.com>
2022-11-17 21:34:51 +0100

    test: test configuration

--- jest.config.ts
+++ jest.config.ts
@@ -3,14 +3,19 @@ import { Config } from '@jest/types';
 const config: Config.InitialOptions = {
   preset: 'ts-jest',
   transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
-  },
-  globals: {
-    'ts-jest': {
+    '^.+\\.(ts|tsx)$': ['ts-jest', {
       isolatedModules: true,
-    },
+    }],
   },
-  resolver: "jest-webpack-resolver"
+  setupFiles: [
+    './setupJest.ts'
+  ],
+  moduleNameMapper: {
+    '^.+\\.(css|scss)$': 'babel-jest'
+  },
+  resolver: 'jest-webpack-resolver',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
 };
 
 export default config;
